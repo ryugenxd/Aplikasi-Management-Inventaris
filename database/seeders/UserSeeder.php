c11ed53a454bdfcb97bc227f1192dea2f6f01db9
ryugenxd <Anonimous7770@gmail.com>
2024-04-08 15:27:45 +0800

    feat: add description application

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,12 +14,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $role_admin = Role::where('name','super_admin')-> first();
+        $role_super_admin = Role::where('name','super_admin')-> first();
+        $role_admin = Role::where('name','admin')->first();
+        $role_staff = Role::where('name','staff')->first();
         // dd($role_admin->id);
+        User::create([
+            "name" => "ryugen",
+            "username" => "ryugen",
+            "role_id" => $role_staff -> id,
+            "password" => bcrypt('12345678')
+        ]);
+        User::create([
+            "name" => "admin",
+            "username" => "admin",
+            "role_id" => $role_admin -> id,
+            "password" => bcrypt('12345678')
+        ]);
         User::create([
             "name" => "super admin",
             "username" => "super_admin",
-            "role_id" => $role_admin -> id,
+            "role_id" => $role_super_admin -> id,
             "password" => bcrypt('12345678')
         ]);
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="aplikasi menejemen inventaris barang">
   <meta name="csrf-token" content="{{csrf_token()}}">
   <title>{{config('app.name')}} | @yield('title')</title>
-
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -97,7 +97,6 @@
 <script src="{{asset('theme/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('theme/dist/js/adminlte.js')}}"></script>
-
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="{{asset('theme/dist/js/pages/dashboard.js')}}"></script> -->
 </body>
