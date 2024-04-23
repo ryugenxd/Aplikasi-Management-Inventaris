a73100e197ec2ce7265837226cbeffe55934b77d
ryugenxd <anonimous7770@gmail.com>
2024-04-23 19:43:21 +0800

    feat: CategoryTest

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_create_success(): void
+    {
+
+    }
+}
