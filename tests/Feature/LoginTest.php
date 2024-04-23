7b53185549a3f2eb28b11c349fd236869b28ea69
ryugenxd <anonimous7770@gmail.com>
2024-04-23 11:03:52 +0800

    refactor: unitest

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -13,7 +13,28 @@ class LoginTest extends TestCase
     /**
      * A basic feature test example.
      */
+
     public function test_login_success(): void
+    {
+        $role = Role::where('name','super_admin')->get()->first();
+        $user_test = [
+            "name"=>"test",
+            "username" => "user_test",
+            "password" => bcrypt("12345678") ,
+            "role_id" =>  $role -> id
+        ];
+        User::create($user_test);
+        $this -> post('/',[
+            'username'=>$user_test["username"],
+            'password'=>'12345678'
+        ])->assertStatus(200)
+        ->assertJson([
+            "success"=>true,
+            "message"=>"Login Berhasil !"
+        ]);
+    }
+
+    public function test_login_failed():void
     {
         $role = Role::where('name','super_admin')->get()->first();
         $user_test = [
@@ -24,14 +45,13 @@ class LoginTest extends TestCase
         ];
 
         User::create($user_test);
-
         $this -> post('/',[
             'username'=>$user_test["username"],
-            'password'=>'12345678'
-        ])->assertStatus(200)
+            'password'=>'345446464'
+        ])->assertStatus(401)
         ->assertJson([
-            "success"=>true,
-            "message"=>"Login Berhasil !"
+            "success"=>false,
+            "message"=>"Username Atau Password Salah !"
         ]);
     }
 }
